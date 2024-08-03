492d1613abb534542c279e1a16f24f69b2c4047d
Dev-Abdelazim <dev.abdelazim@gmail.com>
2024-08-04 00:59:06 +0300

    create styles file and add titel to home view and add google fonts package

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/feature_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -8,9 +9,17 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
         FeatureBooksListView(),
+        Padding(
+          padding: EdgeInsets.only(top: 40, bottom: 16, left: 16, right: 16),
+          child: Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+        )
       ],
     );
   }
